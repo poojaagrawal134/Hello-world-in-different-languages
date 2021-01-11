174d2a42fc8568fb471cf648b7378e3ff0681375
pooja agrawal <pooja.agrawal@austere.systems>
2021-01-11 10:20:12 +0530

    hello world

---
+++ helloworld.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloWorld
+{
+    class Hello {
+        static void Main(string[] args)
+        {      // prints hello world
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
